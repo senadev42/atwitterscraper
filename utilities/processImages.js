01f0f1b92b28ed1f7af99d3d2643c28689699c42
senadev42 <senadevv42@gmail.com>
2024-04-21 08:19:20 +0300

    downloading images locally

---
+++ utilities/processImages.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+import fs from "fs";
+import path from "path";
+
+export async function processImages(tweetObjects) {
+  const downloadDir = "./downloaded_images";
+  if (!fs.existsSync(downloadDir)) {
+    fs.mkdirSync(downloadDir);
+  }
+
+  const tweetswithImages = tweetObjects.filter(
+    (tweet) => tweet.imageURL != null
+  );
+  console.log(`Found ${tweetswithImages.length} tweets with image urls.`);
+
+  for (const tweet of tweetswithImages) {
+    try {
+      console.log(`Downloading image for tweet: ${tweet.imageURL}`);
+
+      //download the image
+      const response = await axios.get(tweet.imageURL, {
+        responseType: "stream",
+      });
+
+      //check type
+      const imageformat = response.headers["content-type"].split("/")[1];
+
+      //save the image
+      const filePath = path.join(
+        downloadDir,
+        path.basename(tweet.imageURL.split("?")[0] + "." + imageformat)
+      );
+      const writer = fs.createWriteStream(filePath);
+
+      response.data.pipe(writer);
+
+      await new Promise((resolve, reject) => {
+        writer.on("finish", resolve);
+        writer.on("error", reject);
+      });
+
+      console.log(`Image downloaded: ${filePath}`);
+    } catch (error) {
+      console.error(
+        `Failed to download image for tweet: ${tweet.imageURL}`,
+        error
+      );
+    }
+  }
+}
+
+// Test
+// const tweetObjects = [
+//  { ImageUrl: 'https://pbs.twimg.com/media/GLlIMhBXYAAd89J?format=jpg&name=small',
+// hash:'tryuimnuibyuvtyretrcytvuybiunjom' },
+// ];
+
+// processImages(tweetObjects).then(() => console.log('All images downloaded.'));
